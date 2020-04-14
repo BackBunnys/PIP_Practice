54921a64c2d4c0e1806e8f83d436954dbfa28b4c
Pylenkov_R <solodov1994roma@gmail.com>
2020-04-14 21:20:30 +0300

    Little hierarchy changes

--- LaboratoryWork-06/src/main/java/com/vlsu/studies/App.java
+++ LaboratoryWork-06/src/main/java/com/vlsu/studies/App.java
@@ -1,27 +1,29 @@
 package com.vlsu.studies;
 
-import com.vlsu.studies.tree.*;
+import com.vlsu.studies.graph.INode;
+import com.vlsu.studies.graph.SimpleNode;
+import com.vlsu.studies.graph.tree.logicalExpressions.*;
 
 public class App {
 
     public void run()
     {
-        INode Expression1 = new ConjunctionNode(
+        INode expression1 = new ConjunctionNode(
                 new ConjunctionNode(
                     new SimpleNode<Boolean>(true),
                     new NegationNode(new SimpleNode<Boolean>(false))),
                 new NegationNode(new SimpleNode<Boolean>(true))
         );
 
-        System.out.println(Expression1 + " = " + Expression1.calculate());
+        System.out.println(expression1 + " = " + expression1.calculate());
 
-        INode Expression2 = new NegationNode(
+        INode expression2 = new NegationNode(
                 new ConjunctionNode(
                     new NegationNode(new SimpleNode<Boolean>(true)),
                     new NegationNode(new SimpleNode<Boolean>(false)))
         );
 
-        System.out.println(Expression2 + " = " + Expression2.calculate());
+        System.out.println(expression2 + " = " + expression2.calculate());
     }
 
 }
--- LaboratoryWork-06/src/main/java/com/vlsu/studies/tree/INode.java
+++
@@ -1,5 +0,0 @@
-package com.vlsu.studies.tree;
-
-public interface INode<T> {
-    T calculate();
-}
